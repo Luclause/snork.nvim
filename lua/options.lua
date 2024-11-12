14b3aba53d5d7eb45521bc4e4c7eb5ba5b4dc337
Luclause <junjiet.ng97@gmail.com>
2024-11-13 01:24:22 +1100

    fix: Remove Powershell setting from master (linux)

--- lua/options.lua
+++ lua/options.lua
@@ -64,12 +64,4 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
--- Configure powershell 
-vim.o.shell = "powershell"
-vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-vim.o.shellquote = ""
-vim.o.shellxquote = ""
-
 -- vim: ts=2 sts=2 sw=2 et
