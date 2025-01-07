9c3c4e7da08cf4ec2999a1df460cebf45e187d19
Luclause <junjiet.ng97@gmail.com>
2025-01-07 17:55:49 +1100

    theme: change to nord
    lazylock

--- lua/plugins/astrocommunity.lua
+++ lua/plugins/astrocommunity.lua
@@ -1,4 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.nordic-nvim" },
+  { import = "astrocommunity.colorscheme.tokyodark-nvim" },
+  { import = "astrocommunity.colorscheme.nord-nvim" },
 }
