87ece0cd0c3744da1840f16f5a2285309b7c03c6
Luclause <junjiet.ng97@gmail.com>
2025-01-09 23:13:46 +1100

    astrolsp: add format_on_save to python, terraform, and json

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -20,6 +20,9 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
+          "python",
+          "terraform",
+          "json",
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
