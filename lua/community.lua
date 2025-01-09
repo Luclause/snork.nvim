c2309846e8a882661ff373b20b61f353a1c88b44
Luclause <junjiet.ng97@gmail.com>
2025-01-10 00:11:50 +1100

    theme: catppuccin
    theme: wtf catppuccin why no transparent pls

--- lua/community.lua
+++ lua/community.lua
@@ -14,6 +14,9 @@ return {
   { import = "astrocommunity.colorscheme.nordic-nvim" },
   { import = "astrocommunity.colorscheme.tokyodark-nvim" },
   { import = "astrocommunity.colorscheme.nord-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
 
   -- Diagnostics
   { import = "astrocommunity.diagnostics.trouble-nvim" },
