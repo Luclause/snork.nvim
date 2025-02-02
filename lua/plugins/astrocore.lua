ea8314771b4f5dba67270262c6627c5e5554691e
Luclause <junjiet.ng97@gmail.com>
2025-02-03 09:20:49 +1100

    astrocore: formatting

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -30,6 +30,8 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        tabstop = 2, -- 2 spaces for tabs (prettier default)
+        shiftwidth = 2, -- 2 spaces for indent width
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
